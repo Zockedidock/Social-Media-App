b9178c571917336d20f30d97cea768f4a56c68ef
Zockedidock <zockedidockhd@gmail.com>
2021-12-22 21:38:14 +0100

    - added Laravel

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>App</title>
+</head>
+<body>
+    @yield('content')
+</body>
+</html>
